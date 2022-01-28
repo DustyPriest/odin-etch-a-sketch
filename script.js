fa6a17d7327cfffc74349788cc44ffb09cacc2cb
DustyPriest <hugo.a.obe@gmail.com>
2022-01-28 13:54:33 +0800

    Clear btn & styling

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-// main grid container
-const grid = document.querySelector('#etch-grid');
+let size = 16;
 
-// create 16x16 divs
-for (let i = 0; i < 16; i++) {
-  for (let i = 0; i < 16; i++) {
+const grid = document.querySelector('#etch-grid');
+const clearBtn = document.querySelector('.clear-btn');
+
+// ------ EVENT LISTENERS ------
+clearBtn.addEventListener('click', clearGrid);
+
+// create SIZExSIZE divs & click events
+for (let i = 0; i < size; i++) {
+  for (let i = 0; i < size; i++) {
     const pixel = document.createElement('div');
     pixel.classList.add('pixel');
+    pixel.style.backgroundColor = 'white';
     pixel.addEventListener('mouseover', etchPixel);
     grid.appendChild(pixel);
   }
 }
 
+// ------ EVENT FUNCTIONS ------
 function etchPixel(e) {
   e.target.style.backgroundColor = 'black';
+}
+
+function clearGrid() {
+  grid.childNodes.forEach((pixel) => (pixel.style.backgroundColor = 'white'));
 }
